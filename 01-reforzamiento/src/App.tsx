1c9759cc625bb134e4d1464169f00fefa7906774
Luis Alberto Alvarez <smum15973@hotmail.com>
2022-10-10 13:25:22 -0600

    First Commit
    These two folders have the a reforcement and unfinished JS and ReactJS practices, but since these are related to ReacJS. Course that it has been already reviewed or at least in process, so there was no need to finish in those practices. Second folder is just a test project to validate the use of device emulators.

---
+++ 01-reforzamiento/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div className='mt-2'>
+      <h1>Intro React Native with Typescript</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ AwesomeTSProject/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text> Hello friend... :3 </Text>
+      <Text> sd;lkfj;alsdjfoaisjfpoasij;asdjfoasdahfpoasidfoasaerhgosdoeirjgoijg;lsdfbo34hfoerijg </Text>
+    </View>
+  );
+};
+
+export default App;
